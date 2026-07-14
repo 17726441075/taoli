53aafd62a18ebcf1dc6933c7f7462d38e4c96392
17726441075 <17726441075@189.cn>
2026-07-14 20:39:47 +0800

    定时任务线程池配置成功

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,7 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
 /***
  * 
  * ssh -i D:\pzl\pzl.pem root@47.91.31.90
@@ -12,9 +15,22 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * 
  * scp -i D:\pzl\pzl.pem .\target\taoli-1.0.jar root@47.91.31.90:~
  * **/
+
+
 @EnableScheduling
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) { SpringApplication.run(Main.class, args); }
 }
 
+@Slf4j
+@Service
+class Test {
+
+    @Scheduled(fixedRate = 100)
+    public void test(){
+        log.info("tets");
+        return ;
+    }
+    
+}
