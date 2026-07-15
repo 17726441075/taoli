9052b83be181b044fa9a55bd832cf4272df9ca91
17726441075 <17726441075@189.cn>
2026-07-15 10:18:25 +0800

    准备开始初始化

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -115,11 +115,15 @@ class AllConfig {
 @Order(0)
 @Slf4j
 @Service
-class DataService {
+class DataService implements ApplicationRunner{
 
     @JSONField(serialize = false)
     public static final EnumMap<Exchange,Map<String,Map<Ticker,BigDecimal>>> futures = new EnumMap<>(Exchange.class) ;
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
 
 }
 @Slf4j
